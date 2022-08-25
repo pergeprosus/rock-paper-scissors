be87ec5491e25cf9244e6fddfe77837b509b761f
Keith Floyd <keithfloyd2001@gmail.com>
2022-08-25 12:48:53 -0400

    Add code to first part of script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-function getComputerChoice(rock, paper, scissors)
+//Pseudocode
+
+//Create a function called getComputerChoice that randomly
+//picks between rock paper or scissors. Ensure that the random picking
+//system works with consolelogs before making any other portions of the game.
+
+let choices = ['rock','paper','scissors']
+
+function getComputerChoice(computerSelection) {
+
+}
+
+
+
+
+
+
+
+
+// Create a function that actually plays the round. It will include two
+//elements called playerSelection (NOT CASE SENSITIVE) and computerSelection, followed by
+//a string that displays the winner and how they won (i.e rock over scissors)
+
+
+
+
+
+
+
+
+
+
+
+
+
