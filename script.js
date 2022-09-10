e754c81489f47c93d469f079b3c9b1120645c268
Keith Floyd <keithfloyd2001@gmail.com>
2022-09-10 09:52:31 -0400

    Add functional button for playing round

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let pointComputer = document.getElementById("botscorenumber");
 //Create a function called getComputerChoice that randomly
 //picks between rock paper or scissors. Ensure that the random picking
 //system works with consolelogs before making any other portions of the game.
-let choices = ['rock', 'paper', 'scissors']
+let choices = ['rock', 'paper', 'scissors'];
 function getComputerChoice(computerSelection) {
     let random = Math.floor(Math.random() * choices.length);
     computerSelection = choices[random];
@@ -30,74 +30,72 @@ function getPlayerChoice(playerSelection) {
     playerSelection = lowercase;
     return playerSelection;
 }
+
 // Create a function that actually plays the round. It will include two
 //elements called playerSelection (NOT CASE SENSITIVE) and computerSelection, followed by
 //a string that displays the winner and how they won (i.e rock over scissors)
 let playerPoints = 0;
 let computerPoints = 0;
 function playRound() {
-    
-    pointPlayer.innerHTML = (" " + playerPoints + "");
-    pointComputer.innerHTML = (" " + computerPoints + "");
+  let  computerSelection = getComputerChoice();
+   let playerSelection = getPlayerChoice();
     let win = "You win this round.";
     let lose = "You lose this round.";
     let draw = "Nobody wins this round."
     //Output for a tie
     if (computerSelection == playerSelection) {
-        playerPoints++;
         resoot.innerHTML = ("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + draw + "");
+        playerPoints++;
         return;
     }
     else;
     //Outputs for rock and scissors
     if (computerSelection == 'rock' && playerSelection == 'scissors') {
         computerPoints++;
-        resoot.innerHTML =("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + lose + "");
+        resoot.innerHTML = ("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + lose + "");
         return;
     }
     else;
     if (computerSelection == 'scissors' && playerSelection == 'rock') {
         playerPoints++;
-        resoot.innerHTML =("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + win + "");
+        resoot.innerHTML = ("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + win + "");
         return;
     }
     else;
     //Outputs for rock and paper
     if (computerSelection == 'rock' && playerSelection == 'paper') {
         playerPoints++;
-        resoot.innerHTML =("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + win + "");
+        resoot.innerHTML = ("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + win + "");
         return;
     }
     else;
     if (computerSelection == 'paper' && playerSelection == 'rock') {
         computerPoints++;
-        resoot.innerHTML =("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + lose + "");
+        resoot.innerHTML = ("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + lose + "");
         return;
     }
     else;
     //Outputs for paper and scissors
     if (computerSelection == 'paper' && playerSelection == 'scissors') {
         playerPoints++;
-        resoot.innerHTML =("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + win + "");
+        resoot.innerHTML = ("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + win + "");
         return;
     }
     else;
     if (computerSelection == 'scissors' && playerSelection == 'paper') {
         computerPoints++;
-        resoot.innerHTML =("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + lose + "");
+        resoot.innerHTML = ("You chose " + playerSelection + ", and the computer chose " + computerSelection + ". " + lose + "");
         return;
     }
     else;
 
 }
 // Give definitions to the parameters which execute the functions to obtain each number
-computerSelection = getComputerChoice();
-playerSelection = getPlayerChoice();
 function game() {
-        playRound();
-        computerSelection = getComputerChoice();
-        playerSelection = getPlayerChoice();
-    
+    playRound();
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerChoice();
+
     let win = "You win!";
     let lose = "You lose!";
     let result;
@@ -109,8 +107,8 @@ function game() {
     }
     console.log("You won " + playerPoints + " rounds, and the computer won " + computerPoints + " rounds. " + result + "")
 }
-game();
-
+pointPlayer.innerHTML = (" " + playerPoints + "");
+pointComputer.innerHTML = (" " + computerPoints + "");
 
 
 
